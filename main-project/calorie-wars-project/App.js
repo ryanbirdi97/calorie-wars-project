dad6a707d3f786117b3d4ec5a91ad07e597ddfcd
Manita Gupta <mannie.gupta@gmail.com>
2022-08-08 19:36:15 +0530

    create home and login page

--- main-project/calorie-wars-project/App.js
+++ main-project/calorie-wars-project/App.js
@@ -1,19 +1,21 @@
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./Components/Login";
-import { Provider as PaperProvider } from 'react-native-paper';
+import Login from "./Screens/Login";
+import Home from "./Screens/Home";
+
+import { Provider as PaperProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
