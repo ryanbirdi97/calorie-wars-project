5deb878807888ae88972f221876369716bb2dddc
ryanbirdi97 <ryanbirdi97@icloud.com>
2022-08-08 12:04:20 +0100

    react native paper installed

--- main-project/calorie-wars-project/App.js
+++ main-project/calorie-wars-project/App.js
@@ -2,15 +2,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./Components/Login";
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <PaperProvider>
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
+    </PaperProvider>
   );
 }
