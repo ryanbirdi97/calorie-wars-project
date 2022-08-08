3a72fcb759dd655baa14cbf4e77e297769f65fea
asad <asadali.asif.1@gmail.com>
2022-08-08 15:43:27 +0100

    implement barcode scanning function

--- main-project/calorie-wars-project/App.js
+++ main-project/calorie-wars-project/App.js
@@ -1,19 +1,18 @@
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./Components/Login";
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider } from "react-native-paper";
+import ScanBarcode from "./Components/ScanBarcode";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <PaperProvider>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ScanBarcode" component={ScanBarcode} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
--- main-project/calorie-wars-project/Components/Login.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Login() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>My home Screen</Text>
-    </View>
-  );
-}
