15f0da54d2fba1c574374cd5fe5271a90e7f849d
asad <asadali.asif.1@gmail.com>
2022-08-08 16:47:50 +0100

    fetch from api

--- main-project/calorie-wars-project/barcodeLookupAPI.js
+++ main-project/calorie-wars-project/barcodeLookupAPI.js
@@ -4,8 +4,31 @@ const barcodeLookupAPI = axios.create({
   baseURL: "https://world.openfoodfacts.org/api/v2/search/",
 });
 
-export const searchProductByBarcode = (barcode) => {
-  barcodeLookupAPI.get(barcode).then((resp) => {
-    return resp.data.products[0].product_name;
+const searchProductByBarcode = (barcode) => {
+  return barcodeLookupAPI.get(barcode).then((resp) => {
+    for (let i = 0; i < Object.keys(resp.data.products[0]).length; i++) {
+      if (Object.keys(resp.data.products[0])[i].match(/ser/i)) {
+        //console.log(Object.keys(resp.data.products[0])[i]);
+      }
+    }
+    //console.log(Object.keys(resp.data.products[0]));
+    /*
+    energy_unit: 'kcal',
+    energy_value: 2,
+    */
+    const obj = resp.data.products[0];
+    const retObj = {
+      product_name: obj.product_name,
+      energy: obj.nutriments["energy-kcal_100g"], // energy per 100 g
+      energy_unit: obj.nutriments.energy_unit,
+      serving_size: obj.serving_size,
+    };
+    console.log(retObj);
+    console.log(
+      "Total calories consumed: ",
+      (retObj.energy * +retObj.serving_size.replace(/[a-z]+/i, "")) / 100
+    );
   });
 };
+
+const res = searchProductByBarcode("5060335635174");
