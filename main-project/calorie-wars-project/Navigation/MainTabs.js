fcaafbb0013aff46cfe8ef87c9cade1ab4248732
geoMx2022 <mwh17dur@gmail.com>
2022-08-09 17:08:39 +0100

    add screen pages and navigation between pages

---
+++ main-project/calorie-wars-project/Navigation/MainTabs.js
@@ -0,0 +1,63 @@
+import * as react from 'react';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { Image } from 'react-native';
+import Home from '../Screens/Home';
+import Leaderboard from '../Screens/Leaderboard';
+import ProfilePage from '../Screens/ProfilePage';
+
+const Tab = createMaterialBottomTabNavigator();
+
+export default function MainTabs() {
+  return (
+    <Tab.Navigator initialRouteName={'Home'}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../assets/home.png')}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Leaderboard"
+        component={Leaderboard}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../assets/leaderboard.png')}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfilePage}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <Image
+              source={require('../assets/user.png')}
+              resizeMode="contain"
+              style={{
+                width: 30,
+                height: 30,
+              }}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
--- main-project/calorie-wars-project/Screens/Login.js
+++ main-project/calorie-wars-project/Screens/Login.js
@@ -1,6 +1,5 @@
-import { useNavigation } from "@react-navigation/core";
-import React, { useEffect, useState } from "react";
-import { Provider as PaperProvider } from "react-native-paper";
+import { useNavigation } from '@react-navigation/core';
+import React, { useEffect, useState } from 'react';
 
 import {
   KeyboardAvoidingView,
@@ -9,13 +8,13 @@ import {
   TouchableOpacity,
   View,
   StyleSheet,
-} from "react-native";
-import { auth } from "../firebase";
+} from 'react-native';
+import { auth } from '../firebase';
 
-export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [page, setPage] = useState("Login");
+export default function Login({ setLoadPage }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [page, setPage] = useState('Login');
 
   const navigation = useNavigation();
 
@@ -35,8 +34,9 @@ export default function Login() {
       .createUserWithEmailAndPassword(email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
-        setPage("ProfilePage");
-        console.log("Registered with:", user.email);
+        setPage('MainTabs');
+        navigation.navigate('MainTabs');
+        console.log('Registered with:', user.email);
       })
       .catch((error) => alert(error.message));
   };
@@ -46,9 +46,9 @@ export default function Login() {
       .signInWithEmailAndPassword(email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
-        setPage("Home");
-        navigation.navigate("Home");
-        console.log("Logged in with:", user.email);
+        setPage('MainTabs');
+        navigation.navigate('MainTabs');
+        console.log('Logged in with:', user.email);
       })
       .catch((error) => alert(error.message));
   };
@@ -75,10 +75,7 @@ export default function Login() {
         <TouchableOpacity onPress={handleLogin} style={styles.button}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={handleSignUp}
-          style={[styles.button, styles.buttonOutline]}
-        >
+        <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
           <Text style={styles.buttonOutlineText}>Register</Text>
         </TouchableOpacity>
       </View>
@@ -89,46 +86,46 @@ export default function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   inputContainer: {
-    width: "80%",
+    width: '80%',
   },
   input: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
     marginTop: 5,
   },
   buttonContainer: {
-    width: "60%",
-    justifyContent: "center",
-    alignItems: "center",
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 40,
   },
   button: {
-    backgroundColor: "#0782F9",
-    width: "100%",
+    backgroundColor: '#0782F9',
+    width: '100%',
     padding: 15,
     borderRadius: 10,
-    alignItems: "center",
+    alignItems: 'center',
   },
   buttonOutline: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     marginTop: 5,
-    borderColor: "#0782F9",
+    borderColor: '#0782F9',
     borderWidth: 2,
   },
   buttonText: {
-    color: "white",
-    fontWeight: "700",
+    color: 'white',
+    fontWeight: '700',
     fontSize: 16,
   },
   buttonOutlineText: {
-    color: "#0782F9",
-    fontWeight: "700",
+    color: '#0782F9',
+    fontWeight: '700',
     fontSize: 16,
   },
 });
