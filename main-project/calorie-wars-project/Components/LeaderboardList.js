e663c396d092392d4abcedd6c69861d3451f27bb
geoMx2022 <mwh17dur@gmail.com>
2022-08-13 15:58:17 +0100

    test code

--- main-project/calorie-wars-project/Components/LeaderboardList.js
+++ main-project/calorie-wars-project/Components/LeaderboardList.js
@@ -6,15 +6,23 @@ import LeaderboardCard from './LeaderboardCard';
 import { v4 as uuidv4 } from 'uuid';
 
 export default LeaderboardList = () => {
-  const [targetCalsGoal, setTargetCalsGoal] = useState(0);
-  const [targetStepsGoal, setTargetStepsGoal] = useState(0);
-  const [currentCals, setCurrentCals] = useState(0);
-  const [currentSteps, setCurrentSteps] = useState(0);
-  const [username, setUsername] = useState('');
+  console.log('This is a test on leaderboard2');
+
+  //const [targetCalsGoal, setTargetCalsGoal] = useState(0);
+  //const [targetStepsGoal, setTargetStepsGoal] = useState(0);
+  //const [currentCals, setCurrentCals] = useState(0);
+  //const [currentSteps, setCurrentSteps] = useState(0);
+  //const [username, setUsername] = useState('');
+  const [leaderboard, setLeaderboard] = useState([]);
 
   let scoreCals = 0;
   let scoreSteps = 0;
   let rank = 1;
+  let targetCalsGoal = 0;
+  let targetStepsGoal = 0;
+  let currentCals = 0;
+  let currentSteps = 0;
+  let username = '';
 
   const email = auth.currentUser?.email;
   const getUserEmail = db.collection('users').doc(auth.currentUser?.email);
@@ -23,20 +31,20 @@ export default LeaderboardList = () => {
     .collection('goals')
     .doc(email + '-goals')
     .onSnapshot((doc) => {
-      setTargetCalsGoal(doc.data().calorie_goal);
-      setTargetStepsGoal(doc.data().step_goal);
+      targetCalsGoal = doc.data().calorie_goal;
+      targetStepsGoal = doc.data().step_goal;
     });
 
   getUserEmail
     .collection('cals_step_log')
     .doc(email + '-cal_step_log')
     .onSnapshot((doc) => {
-      setCurrentCals(doc.data().cals_consumed);
-      setCurrentSteps(doc.data().steps);
+      currentCals = doc.data().cals_consumed;
+      currentSteps = doc.data().steps;
     });
 
   getUserEmail.onSnapshot((doc) => {
-    setUsername(doc.data().username);
+    username = doc.data().username;
   });
 
   if (currentCals < targetCalsGoal) {
@@ -51,25 +59,29 @@ export default LeaderboardList = () => {
   else scoreSteps = 50;
 
   const score = Number(scoreCals) + Number(scoreSteps);
-  const leaderboardList = [];
 
-  const [leaderboard, setLeaderboard] = useState([]);
+  let leaderboardList = [];
 
   db.collectionGroup('leaderboard')
     .get()
     .then((querySnapShot) => {
-      querySnapShot.forEach((doc) => {
-        leaderboardList.push(doc.data());
+      //leaderboardList = { ...querySnapShot };
+      return querySnapShot.forEach((doc) => {
+        return leaderboardList.push(doc.data());
+        //setLeaderboard((currentLeaderboard) => {
+        //return [...currentLeaderboard, doc.data()];
       });
-      setLeaderboard(leaderboardList);
-    });
+    })
+    .then((doc) => console.log(doc));
 
-  console.log(leaderboard, '<----');
+  //console.log(leaderboard, '<------');
+
   return (
     <View>
-      {leaderboard.map((obj) => (
+      <Text>Hello</Text>
+      {/*{leaderboard.map((obj) => (
         <LeaderboardCard key={uuidv4()} obj={obj} />
-      ))}
+      ))}*/}
     </View>
   );
 };
