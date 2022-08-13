694ec6c43e98d8f9a80820afaaed2770facd644b
geoMx2022 <mwh17dur@gmail.com>
2022-08-13 16:38:57 +0100

    leaderboardList no infinite loop but no live update

--- main-project/calorie-wars-project/Components/LeaderboardList.js
+++ main-project/calorie-wars-project/Components/LeaderboardList.js
@@ -1,51 +1,52 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db, auth } from '../firebase';
 import LeaderboardCard from './LeaderboardCard';
 import { v4 as uuidv4 } from 'uuid';
 
 export default LeaderboardList = () => {
-  console.log('This is a test on leaderboard2');
-
-  //const [targetCalsGoal, setTargetCalsGoal] = useState(0);
-  //const [targetStepsGoal, setTargetStepsGoal] = useState(0);
-  //const [currentCals, setCurrentCals] = useState(0);
-  //const [currentSteps, setCurrentSteps] = useState(0);
-  //const [username, setUsername] = useState('');
+  const [targetCalsGoal, setTargetCalsGoal] = useState(0);
+  const [targetStepsGoal, setTargetStepsGoal] = useState(0);
+  const [currentCals, setCurrentCals] = useState(0);
+  const [currentSteps, setCurrentSteps] = useState(0);
+  const [username, setUsername] = useState('');
   const [leaderboard, setLeaderboard] = useState([]);
 
   let scoreCals = 0;
   let scoreSteps = 0;
   let rank = 1;
-  let targetCalsGoal = 0;
-  let targetStepsGoal = 0;
-  let currentCals = 0;
-  let currentSteps = 0;
-  let username = '';
 
   const email = auth.currentUser?.email;
   const getUserEmail = db.collection('users').doc(auth.currentUser?.email);
 
-  getUserEmail
-    .collection('goals')
-    .doc(email + '-goals')
-    .onSnapshot((doc) => {
-      targetCalsGoal = doc.data().calorie_goal;
-      targetStepsGoal = doc.data().step_goal;
-    });
+  console.log('hello');
 
-  getUserEmail
-    .collection('cals_step_log')
-    .doc(email + '-cal_step_log')
-    .onSnapshot((doc) => {
-      currentCals = doc.data().cals_consumed;
-      currentSteps = doc.data().steps;
-    });
+  useEffect(() => {
+    getUserEmail
+      .collection('goals')
+      .doc(email + '-goals')
+      .onSnapshot((doc) => {
+        setTargetCalsGoal(doc.data().calorie_goal);
+        setTargetStepsGoal(doc.data().step_goal);
+      });
+  }, [targetCalsGoal, targetStepsGoal]);
 
-  getUserEmail.onSnapshot((doc) => {
-    username = doc.data().username;
-  });
+  useEffect(() => {
+    getUserEmail
+      .collection('cals_step_log')
+      .doc(email + '-cal_step_log')
+      .onSnapshot((doc) => {
+        setCurrentCals(doc.data().cals_consumed);
+        setCurrentSteps(doc.data().steps);
+      });
+  }, []);
+
+  useEffect(() => {
+    getUserEmail.onSnapshot((doc) => {
+      setUsername(doc.data().username);
+    });
+  }, []);
 
   if (currentCals < targetCalsGoal) {
     scoreCals = ((currentCals / targetCalsGoal) * 50).toFixed(2);
@@ -60,28 +61,24 @@ export default LeaderboardList = () => {
 
   const score = Number(scoreCals) + Number(scoreSteps);
 
-  let leaderboardList = [];
-
-  db.collectionGroup('leaderboard')
-    .get()
-    .then((querySnapShot) => {
-      //leaderboardList = { ...querySnapShot };
-      return querySnapShot.forEach((doc) => {
-        return leaderboardList.push(doc.data());
-        //setLeaderboard((currentLeaderboard) => {
-        //return [...currentLeaderboard, doc.data()];
+  useEffect(() => {
+    db.collectionGroup('leaderboard')
+      .get()
+      .then((querySnapShot) => {
+        const leaderboardList = [];
+        querySnapShot.forEach((doc) => {
+          leaderboardList.push(doc.data());
+        });
+        setLeaderboard(leaderboardList);
       });
-    })
-    .then((doc) => console.log(doc));
-
-  //console.log(leaderboard, '<------');
+  }, []);
 
   return (
     <View>
       <Text>Hello</Text>
-      {/*{leaderboard.map((obj) => (
+      {leaderboard.map((obj) => (
         <LeaderboardCard key={uuidv4()} obj={obj} />
-      ))}*/}
+      ))}
     </View>
   );
 };
