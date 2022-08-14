79d09e53a322b64a8977b0a4b0385892c5268c87
Manita Gupta <mannie.gupta@gmail.com>
2022-08-14 10:17:00 +0530

    see live updates on leaderboard

--- main-project/calorie-wars-project/Components/LeaderboardList.js
+++ main-project/calorie-wars-project/Components/LeaderboardList.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db, auth } from '../firebase';
 import LeaderboardCard from './LeaderboardCard';
 import { v4 as uuidv4 } from 'uuid';
@@ -11,6 +11,7 @@ export default LeaderboardList = () => {
   const [currentCals, setCurrentCals] = useState(0);
   const [currentSteps, setCurrentSteps] = useState(0);
   const [username, setUsername] = useState('');
+  const [leaderboard, setLeaderboard] = useState([]);
 
   let scoreCals = 0;
   let scoreSteps = 0;
@@ -19,25 +20,31 @@ export default LeaderboardList = () => {
   const email = auth.currentUser?.email;
   const getUserEmail = db.collection('users').doc(auth.currentUser?.email);
 
-  getUserEmail
-    .collection('goals')
-    .doc(email + '-goals')
-    .onSnapshot((doc) => {
-      setTargetCalsGoal(doc.data().calorie_goal);
-      setTargetStepsGoal(doc.data().step_goal);
-    });
+  useEffect(() => {
+    getUserEmail
+      .collection('goals')
+      .doc(email + '-goals')
+      .onSnapshot((doc) => {
+        setTargetCalsGoal(doc.data().calorie_goal);
+        setTargetStepsGoal(doc.data().step_goal);
+      });
+  }, []);
 
-  getUserEmail
-    .collection('cals_step_log')
-    .doc(email + '-cal_step_log')
-    .onSnapshot((doc) => {
-      setCurrentCals(doc.data().cals_consumed);
-      setCurrentSteps(doc.data().steps);
-    });
+  useEffect(() => {
+    getUserEmail
+      .collection('cals_step_log')
+      .doc(email + '-cal_step_log')
+      .onSnapshot((doc) => {
+        setCurrentCals(doc.data().cals_consumed);
+        setCurrentSteps(doc.data().steps);
+      });
+  }, []);
 
-  getUserEmail.onSnapshot((doc) => {
-    setUsername(doc.data().username);
-  });
+  useEffect(() => {
+    getUserEmail.onSnapshot((doc) => {
+      setUsername(doc.data().username);
+    });
+  }, []);
 
   if (currentCals < targetCalsGoal) {
     scoreCals = ((currentCals / targetCalsGoal) * 50).toFixed(2);
@@ -53,25 +60,62 @@ export default LeaderboardList = () => {
   const score = Number(scoreCals) + Number(scoreSteps);
   const leaderboardList = [];
 
-  const [leaderboard, setLeaderboard] = useState([]);
-
+  // useEffect(() => {
   db.collectionGroup('leaderboard')
     .get()
     .then((querySnapShot) => {
+      const leaderboardList = [];
       querySnapShot.forEach((doc) => {
         leaderboardList.push(doc.data());
       });
       setLeaderboard(leaderboardList);
     });
+  // }, []);
 
-  console.log(leaderboard, '<----');
+  //console.log(leaderboard, '<----');
   return (
+    // <View style={styles.container}>
     <View>
-      {leaderboard.map((obj) => (
-        <LeaderboardCard key={uuidv4()} obj={obj} />
-      ))}
+      <View style={styles.container}>
+        <View style={styles.cardHeader}>
+          <Text style={styles.baseText}>#</Text>
+          <Text style={styles.baseText}>Username</Text>
+          <Text style={styles.baseText}>Steps to Goal</Text>
+          <Text style={styles.baseText}>Cals to Goal</Text>
+          <Text style={styles.baseText}>Score</Text>
+        </View>
+      </View>
+      <View>
+        {leaderboard.map((obj) => (
+          <LeaderboardCard key={uuidv4()} obj={obj} />
+        ))}
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 55,
+    margin: 5,
+    padding: 2,
+    backgroundColor: '#dee2e6',
+    borderColor: 'black',
+    borderWidth: 2,
+  },
+  cardHeader: {
+    backgroundColor: '#9d0208',
+    margin: 5,
+    padding: 5,
+    borderColor: 'black',
+    borderWidth: 2,
+    fontSize: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 37,
+  },
+  baseText: {
+    fontWeight: '700',
+  },
+});
